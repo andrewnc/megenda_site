a3295a06987aa5909ebce4034984ea577635b542
andrewnc <andrewcarr06@gmail.com>
2017-09-19 19:09:24 -0600

    update requirements

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,5 @@
-SECRET_KEY = 'G(doYAe*V6Ja'
+# SECRET_KEY = 'G(doYAe*V6Ja'
+SECRET_KEY = '89df55cb0b5370e42daeb0071dad444f4632e7b88f65dc56e9b818e03acb2135'
 # wq;*bX!5trcQ temp password
-SQLALCHEMY_DATABASE_URI = 'postgres://localhost/megenda'
+# SQLALCHEMY_DATABASE_URI = 'postgres://localhost/megenda'
+SQLALCHEMY_DATABASE_URI = 'postgres://tncfnvbmhxcccu:89df55cb0b5370e42daeb0071dad444f4632e7b88f65dc56e9b818e03acb2135@ec2-184-73-189-221.compute-1.amazonaws.com:5432/dd6ca07mvpj3ea'
